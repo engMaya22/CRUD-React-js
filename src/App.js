3b5e09eecd7deafce99d6250a0169c1ad473a634
maya <maya.e@webased.net>
2024-09-02 03:45:38 -0700

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  Button,
+  ButtonGroup,
+} from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <Row>
+        <Col xs={{ span: 8, offset: 2 }}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th style={{ width: "70%" }}>Title</th>
+                <th style={{ width: "10%" }}></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>#1</td>
+                <td>this is title 1</td>
+                <td>
+                  <ButtonGroup aria-label="Basic example">
+                    <Button variant="success">Edit</Button>
+                    <Button variant="danger">Delete</Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
